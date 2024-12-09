eea9d5d5ba71eb1b763fc8c517f16e75bdb9b70d
Chris <chris_aitch@fastmail.co.uk>
2024-12-09 22:28:08 +0100

    Cleanup: remove unused flood_fill functions
    They were paart of the path checking that we no longer need.

--- test_to_knows_if_is_playable2.c
+++ test_to_knows_if_is_playable2.c
@@ -19,32 +19,3 @@ t_lib1	*test_to_knows_if_is_playable2(t_lib1 *map_data)
 		map_data->how_many_colums);
 	return (map_data);
 }
-
-// TODO Understand if the flood_fill works for us now
-// What is its purpose and how does it work?
-void	flood_fill(t_lib1 *map_data, int x, int y)
-{
-	if (map_data->cpy_of_map_array[x][y] != '1'
-		&& map_data->cpy_of_map_array[x][y] != 'E'
-		&& map_data->cpy_of_map_array[x][y] != '2')
-	{
-		map_data->cpy_of_map_array[x][y] = '2';
-		flood_fill(map_data, x + 1, y);
-		flood_fill(map_data, x - 1, y);
-		flood_fill(map_data, x, y + 1);
-		flood_fill(map_data, x, y - 1);
-	}
-}
-
-// FIXME What does this do different from the other flood_fill?
-void	flood_fill1(t_lib1 *map_data, int x, int y)
-{
-	if (map_data->cpy_of_map_array[x][y] != '1')
-	{
-		map_data->cpy_of_map_array[x][y] = '1';
-		flood_fill1(map_data, x + 1, y);
-		flood_fill1(map_data, x - 1, y);
-		flood_fill1(map_data, x, y + 1);
-		flood_fill1(map_data, x, y - 1);
-	}
-}
