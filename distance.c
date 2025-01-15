2b295a00e5df45bd22c5a9ee0c0f7ffdb76e4537
brillande <emedina-@DESKTOP-GHPVO1T>
2025-01-15 17:18:58 +0100

    ternarios quitados

--- distance.c
+++ distance.c
@@ -8,19 +8,27 @@ void dda_alg(t_lib1 *data, t_lib1 *ca) {
             ca->side_x += ca->delta_x;
             ca->map_x += ca->step_x;
             // Determinar el lado del impacto
-            data->player.side = (ca->step_x > 0) ? 0 : 1;
+            if (ca->step_x > 0) {
+                data->player.side = 0;
+            } else {
+                data->player.side = 1;
+            }
         } else {
             ca->side_y += ca->delta_y;
             ca->map_y += ca->step_y;
             // Determinar el lado del impacto
-            data->player.side = (ca->step_y > 0) ? 2 : 3;
+            if (ca->step_y > 0) {
+                data->player.side = 2;
+            } else {
+                data->player.side = 3;
+            }
         }
 
         // Verificar límites del mapa
-		ft_printf("map_x: %d, map_y: %d\n", ca->map_x, ca->map_y);
-         if (ca->map_x < 0 || ca->map_x >= data->len_cols || ca->map_y < 0 || ca->map_y >= data->len_rows) {
+        ft_printf("map_x: %d, map_y: %d\n", ca->map_x, ca->map_y);
+        if (ca->map_x < 0 || ca->map_x >= data->len_cols || ca->map_y < 0 || ca->map_y >= data->len_rows) {
             fprintf(stderr, "Error: Out of map bounds\n");
-           break;
+            break;
         }
 
         // Verificar colisión con una pared
