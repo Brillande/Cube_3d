a4d55b5086a7b316700a65bac9be979d8d2d7d80
Chris Haikney <chaikney@c4r4s1.42urduliz.com>
2025-01-16 15:26:55 +0100

    Remove duplicate t_lib from dda_alg
    No problems observed.

--- distance.c
+++ distance.c
@@ -1,23 +1,25 @@
 #include "cube_3d.h"
 
 // TODO Use e_directions instead of cryptic numbers here
-void dda_alg(t_lib1 *data, t_lib1 *ca) {
-    while (ca->hit == 0) {
+void dda_alg(t_lib1 *data, t_lib1 *ca)
+{
+	(void) ca;
+    while (data->hit == 0) {
         // Avanzar en la dirección X o Y
-        if (ca->side_x < ca->side_y) {
-            ca->side_x += ca->delta_x;
-            ca->map_x += ca->step_x;
+        if (data->side_x < data->side_y) {
+            data->side_x += data->delta_x;
+            data->map_x += data->step_x;
             // Determinar el lado del impacto
-            if (ca->step_x > 0) {
+            if (data->step_x > 0) {
                 data->player.side = 0;
             } else {
                 data->player.side = 1;
             }
         } else {
-            ca->side_y += ca->delta_y;
-            ca->map_y += ca->step_y;
+            data->side_y += data->delta_y;
+            data->map_y += data->step_y;
             // Determinar el lado del impacto
-            if (ca->step_y > 0) {
+            if (data->step_y > 0) {
                 data->player.side = 2;
             } else {
                 data->player.side = 3;
@@ -25,16 +27,16 @@ void dda_alg(t_lib1 *data, t_lib1 *ca) {
         }
 
         // Verificar límites del mapa
-        ft_printf("map_x: %d, map_y: %d\n", ca->map_x, ca->map_y);
-        if (ca->map_x < 0 || ca->map_x >= data->len_cols || ca->map_y < 0 || ca->map_y >= data->len_rows) {
+        ft_printf("map_x: %d, map_y: %d\n", data->map_x, data->map_y);
+        if (data->map_x < 0 || data->map_x >= data->len_cols || data->map_y < 0 || data->map_y >= data->len_rows) {
             fprintf(stderr, "Error: Out of map bounds\n");
             break;
         }
 
         // Verificar colisión con una pared
         // NOTE this should be map_array[][] as map is blank
-        if (data->map_array[ca->map_y][ca->map_x] == '1') {
-            ca->hit = 1;
+        if (data->map_array[data->map_y][data->map_x] == '1') {
+            data->hit = 1;
         }
     }
 }
