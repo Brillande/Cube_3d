d53b0faf2c7872b56f48f50c2a9a1d4954fb8d02
Chris <chris_aitch@fastmail.co.uk>
2025-01-16 19:18:49 +0100

    FIX segfault in dda_alg
    * Access x and y coords of map in correct order
    * Remove ineffective test against columns variable we do not use.

--- distance.c
+++ distance.c
@@ -1,43 +1,45 @@
 #include "cube_3d.h"
 
 // TODO Use e_directions instead of cryptic numbers here
+// FIXED Constantly throws "out of bounds" errrors
 void dda_alg(t_lib1 *data)
 {
-    while (data->hit == 0) {
-        // Avanzar en la dirección X o Y
-        if (data->side_x < data->side_y) {
-            data->side_x += data->delta_x;
-            data->map_x += data->step_x;
-            // Determinar el lado del impacto
-            if (data->step_x > 0) {
-                data->player.side = 0;
-            } else {
-                data->player.side = 1;
-            }
-        } else {
-            data->side_y += data->delta_y;
-            data->map_y += data->step_y;
-            // Determinar el lado del impacto
-            if (data->step_y > 0) {
-                data->player.side = 2;
-            } else {
-                data->player.side = 3;
-            }
-        }
-
-        // Verificar límites del mapa
-        ft_printf("map_x: %d, map_y: %d\n", data->map_x, data->map_y);
-        if (data->map_x < 0 || data->map_x >= data->len_cols || data->map_y < 0 || data->map_y >= data->len_rows) {
-            fprintf(stderr, "Error: Out of map bounds\n");
-            break;
-        }
-
-        // Verificar colisión con una pared
-        // NOTE this should be map_array[][] as map is blank
-        if (data->map_array[data->map_y][data->map_x] == '1') {
-            data->hit = 1;
-        }
-    }
+	while (data->hit == 0)
+	{
+		// Avanzar en la dirección X o Y
+		if (data->side_x < data->side_y)
+		{
+			data->side_x += data->delta_x;
+			data->map_x += data->step_x;
+			// Determinar el lado del impacto
+			if (data->step_x > 0)
+				data->player.side = 0;
+			else
+				data->player.side = 1;
+		}
+		else
+		{
+			data->side_y += data->delta_y;
+			data->map_y += data->step_y;
+			// Determinar el lado del impacto
+			if (data->step_y > 0)
+				data->player.side = 2;
+			else
+				data->player.side = 3;
+		}
+		// Verificar límites del mapa
+		ft_printf("map_x: %d, map_y: %d\n", data->map_x, data->map_y);
+		if (data->map_x < 0 || data->map_y < 0 )
+		{
+			fprintf(stderr, "Error: Out of map bounds\n");
+			break;
+		}
+		// Verificar colisión con una pared
+		// NOTE this should be map_array[][] as map is blank
+		if (data->map_array[data->map_x][data->map_y] == '1') {
+			data->hit = 1;
+		}
+	}
 }
 
 // What does this do?
