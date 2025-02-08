71930455111bc96e026f44434a5ee7a5fc26e74b
Chris <chris_aitch@fastmail.co.uk>
2025-02-08 20:44:00 +0100

    Fix in debug printing of texture paths

--- debug.c
+++ debug.c
@@ -16,12 +16,12 @@
 void	print_read_from_file(t_lib1 map_data)
 {
 	ft_printf("Have read file\n");
-	ft_printf("North texture: %s\n", map_data.texture_paths[0]);
-	ft_printf("South texture: %s\n", map_data.texture_paths[1]);
-	ft_printf("East texture: %s\n", map_data.texture_paths[2]);
-	ft_printf("West texture: %s\n", map_data.texture_paths[3]);
-	ft_printf("Floor colour: %i\n", map_data.rgb_floor);
-	ft_printf("Ceiling colour: %i\n", map_data.rgb_ceiling);
+	ft_printf("North texture: %s\n", map_data.texture_paths[NORTH]);
+	ft_printf("South texture: %s\n", map_data.texture_paths[SOUTH]);
+	ft_printf("East texture: %s\n", map_data.texture_paths[EAST]);
+	ft_printf("West texture: %s\n", map_data.texture_paths[WEST]);
+	ft_printf("Floor colour: %x\n", map_data.rgb_floor);
+	ft_printf("Ceiling colour: %x\n", map_data.rgb_ceiling);
 	ft_printf("Map data read:\n%s", map_data.map_content);
 	ft_printf("Sizes:\nX: %i\tY: %i\n", map_data.how_many_colums,
 		map_data.how_many_lines);
