c9ee15232d8b10dda1e17b0c675f3f87bab5f32a
brillande <emedina-@MSI.>
2025-02-20 19:48:51 +0100

    fixing_norm

--- print_3d.c
+++ print_3d.c
@@ -6,7 +6,7 @@
 /*   By: emedina- <emedina-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 17:36:29 by emedina-          #+#    #+#             */
-/*   Updated: 2025/02/20 19:22:53 by emedina-         ###   ########.fr       */
+/*   Updated: 2025/02/20 19:47:35 by emedina-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,52 +21,53 @@ static int	get_rgba(mlx_texture_t *texture, int x, int y)
 	int	g;
 	int	b;
 	int	a;
-    // extrae los valores de los colores de la textura
+
 	r = texture->pixels[y * texture->width * 4 + (x * 4)];
 	g = texture->pixels[y * texture->width * 4 + (x * 4) + 1];
 	b = texture->pixels[y * texture->width * 4 + (x * 4) + 2];
 	a = texture->pixels[y * texture->width * 4 + (x * 4) + 3];
-    // Combina los componentes en un solo valor entero RGBA
 	return (r << 24 | g << 16 | b << 8 | a);
 }
 
 // TODO Give this function a useful name and description
-// esta función llena la estructura info con la información necesaria para dibujar una pared
-void fill_info(t_info *info, t_lib1 *data, double height) {
-    // Asigna la textura correspondiente a la pared
-    info->tex = data->texture[data->player.side];
-
-    // Calcula los valores de top, bottom y text_start
-    if (height >= SCREENHEIGHT) {
-        info->top = 0;
-        info->bottom = SCREENHEIGHT - 1;
-        info->text_start = ((height - SCREENHEIGHT) / 2.0) * info->tex->height / height;
-    } else {
-        info->top = (SCREENHEIGHT - height) / 2;
-        info->bottom = (SCREENHEIGHT + height) / 2;
-        info->text_start = 0.0;
-    }
-
-    // Calcula el paso de la textura en función de la altura de la pared
-    info->step = info->tex->height / height;
+void	fill_info(t_info *info, t_lib1 *data, double height)
+{
+	info->tex = data->texture[data->player.side];
+	if (height >= SCREENHEIGHT)
+	{
+		info->top = 0;
+		info->bottom = SCREENHEIGHT - 1;
+		info->text_start = ((height - SCREENHEIGHT) / 2.0)
+			* info->tex->height / height;
+	}
+	else
+	{
+		info->top = (SCREENHEIGHT - height) / 2;
+		info->bottom = (SCREENHEIGHT + height) / 2;
+		info->text_start = 0.0;
+	}
+	info->step = info->tex->height / height;
 }
 
 // TODO Work out how this relates to the get_background function
 //esta función dibuja el techo y el suelo
-void draw_ceiling_and_floor(t_lib1 *data, int i) {
-    int start = 0;
-    int half = (SCREENHEIGHT - start) / 2;
+void	draw_ceiling_and_floor(t_lib1 *data, int i)
+{
+	int	start;
+	int	half;
 
-    // Dibuja el techo
-    while (start < half) {
-        mlx_put_pixel(data->img, i, start, data->rgb_ceiling);
-        start++;
-    }
-    // Dibuja el suelo
-    while (start < SCREENHEIGHT) {
-        mlx_put_pixel(data->img, i, start, data->rgb_floor);
-        start++;
-    }
+	start = 0;
+	half = SCREENHEIGHT / 2;
+	while (start < half)
+	{
+		mlx_put_pixel(data->img, i, start, data->rgb_ceiling);
+		start++;
+	}
+	while (start < SCREENHEIGHT)
+	{
+		mlx_put_pixel(data->img, i, start, data->rgb_floor);
+		start++;
+	}
 }
 
 // Draw a wall column in a solid colour
@@ -74,46 +75,17 @@ void draw_ceiling_and_floor(t_lib1 *data, int i) {
 // - Calculate the pixel range that should be floor, ceiling and wall
 // - draw down the screen_column in that colour
 // FIXME Too slow! Is there a better way than directly pixel_put?
-void	solid_walls(t_lib1 *data, double distance, int screen_col, mlx_image_t *img)
-{
-	int	line_height;	// integer because it corresponds to screen pixels
-	int	midpoint;	// the middle of the screen where floor / ceiling switch;
-	int	start_point;
-	int	end_point;
-	int	i;	// counter for painting the screen.
-
-	printf("ray value: %f\n", distance);
-	if (distance == 0)
-		line_height = SCREENHEIGHT;
-	else
-		line_height = SCREENHEIGHT / distance;	// implicit conversion to int here
-	midpoint = SCREENHEIGHT / 2;
-	start_point = (-line_height / 2) + midpoint;
-	end_point = (line_height / 2) + midpoint;
-	// Correct for either of these going offscreen (i.e. we are too close to the wall to see its ends)
-	if (start_point < 0)
-		start_point = 0;
-	if (end_point >= SCREENHEIGHT)
-		end_point = SCREENHEIGHT - 1;
-	printf("Drawing a column (%i), start: %i, middle: %i, end: %i, height: %i\n", screen_col, start_point, midpoint, end_point, line_height);
-	i = 1;
-	while (i <= start_point)
-		mlx_put_pixel(img, screen_col, i++, data->rgb_ceiling);	// FIXME Segfualt invalid write
-	while (i <= end_point)
-		mlx_put_pixel(img, screen_col, i++, 0xE0CD9F);	// Randomly picked lime green shade
-	while (i < SCREENHEIGHT - 1)
-		mlx_put_pixel(img, screen_col, i++, data->rgb_floor);
-}
 
 // find the x-coord of the texture corresponding to wall_strike
 // define a step size for us to move up through the texture column
 // Drawing the floor and ceiling is the same as solid_walls() but...
 //
 // TODO Split this oh lord its a mess
-// FIXME Current issues: directly ahead we lose the bottom (grey) and top (cut off) - skewed?
-// FIXME Mirror effect in map.cub simple case - when the ray crosses halfway, RHS is wrong (> player angle)
-// TODO Far too slow - build a column that we write directly?
-#include "cube_3d.h"
+// FIXME Current issues: directly ahead we lose 
+//the bottom (grey) and top (cut off) - skewed?
+// FIXME Mirror effect in map.cub simple case
+// - when the ray crosses halfway, RHS is wrong (> player angle)
+
 
 // Función para dibujar paredes texturizadas
 void textured_walls(t_lib1 *data, int screen_col, mlx_image_t *img, double strike_pt, mlx_texture_t *tex, double distance) {
