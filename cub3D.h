6d183c2fd77facf33a01eeb97cb151fbb8777f11
Chris Haikney <chaikney@c4r5s1.42urduliz.com>
2025-03-25 13:14:18 +0100

    remove create_trgb()
    One line function, replaced with expression in return call.

--- cub3D.h
+++ cub3D.h
@@ -163,7 +163,6 @@ void		setup_camera_plane(double rads, t_player *player);
 // get_colours.c
 int			get_colours(int fd, char key);
 int			get_colours_from_array(char **parts);
-int			create_trgb(int t, int r, int g, int b);
 void		read_colours(t_lib1 *data, int fd);
 
 // file_helpers.c
