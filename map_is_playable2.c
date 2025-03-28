0b4453b6e1c4b3a6e2d55c970c88c38f5410252a
Chris Haikney <chaikney@c4r6s1.42urduliz.com>
2025-03-28 15:03:12 +0100

    Add test when splitting map_string to an array
    It might be ineffective depending on the output of previous file
    reading (i.e. it probably stopped with the blank line that consecutive
    newlines implies), but it can't hurt I reckon.

--- map_is_playable2.c
+++ map_is_playable2.c
@@ -12,15 +12,40 @@
 
 #include "cub3D.h"
 
+// Split the raw map string to an array.
+// Ensure it is safely-structured.
+// Check that there are no consecutive newlines in the string before splitting.
+// ...any other things to protect against?
+static char	**split_map_to_array(char *map_string)
+{
+	int	i;
+
+	i = 0;
+	while (map_string[i + 1] != '\0')
+	{
+		if (map_string[i] == '\n')
+			if (map_string[i + 1] == '\n')
+				return (NULL);
+		i++;
+	}
+	return (ft_split(map_string, '\n'));
+}
+
 // - Check that map_content only has allowed characters and a single player
-// - turn the raw content into map_array using ft_split
+// - turn the raw content into map_array
 // - check to make sure the map is surrounded
+// -- first a quick test
+// -- then a slower, per-square, one
+// - Find the player's position
+// - Setup player start data
 void	map_is_playable(t_lib1 *map_data)
 {
 	if ((!only_legal_char(map_data->map_content))
 		|| (!only_one_player(map_data->map_content)))
 		bad_map(map_data, "Illegal char in map");
-	map_data->map_array = ft_split(map_data->map_content, '\n');
+	map_data->map_array = split_map_to_array(map_data->map_content);
+	if (!map_data->map_array)
+		bad_map(map_data, "Map splitting failed to make a valid array\n");
 	if (!basic_wall_test(map_data))
 		bad_map(map_data, "Wall count error\n");
 	if (!check_each_square(map_data))
